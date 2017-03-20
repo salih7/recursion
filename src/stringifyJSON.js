3e4a512b7f8e8c2fb51cdc7a113c4ae43dc9c4e0
Salih <salih7@gmail.com>
2017-03-20 11:46:38 -0500

    Implement stringifyJSON Function Using Recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,37 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if(Array.isArray(obj)) {
+		var arrStr = [];
+		for(var i = 0; i < obj.length; i++) {
+			arrStr.push(stringifyJSON(obj[i]));	
+		}
+		return '[' + arrStr.join(',') + ']';
+  } else if(obj !== null && obj !== undefined) { 
+  	if(typeof obj === 'object') {
+  		var objArr = [];
+  		var objStr;
+  		for(var i in obj) {
+  			objStr = '';
+  			if(obj[i] !== undefined && typeof obj[i] !== 'function') {
+					objStr += stringifyJSON(i);
+	  			objStr += ':';
+	  			objStr += stringifyJSON(obj[i]);
+	  			objArr.push(objStr);
+  			}
+  		}
+  		return '{' + objArr.join(',') + '}';
+  	} else if(typeof obj === 'string') {
+  		return '"' + obj + '"';		
+ 	  } else if(obj === undefined) {
+ 		 	return '';
+ 	  } else {
+  		console.log(obj);
+  		return obj.toString();
+  	}
+  } else {
+  	if(obj === null) {
+  		return 'null';
+  	}	
+  }
 };
